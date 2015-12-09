3a8b7d66fbca184990b6423b9ad1b47e2c2b2af4
Christoph Guttandin <chrisguttandin@media-codings.com>
2015-12-09 23:48:19 +0100

    import modernizr tests anonymously

--- src/modernizr.js
+++ src/modernizr.js
@@ -1,11 +1,9 @@
 'use strict';
 
 import { annotate } from 'di';
-/* eslint-disable no-unused-vars */
-import promises from 'browsernizr/test/es6/promises';
-import typedArrays from 'browsernizr/test/typed-arrays';
-import webaudio from 'browsernizr/test/audio/webaudio';
-/* eslint-enable no-unused-vars */
+import 'browsernizr/test/audio/webaudio';
+import 'browsernizr/test/es6/promises';
+import 'browsernizr/test/typed-arrays';
 
 // browsernizr needs to be imported after the individual tests
 import modernizr from 'browsernizr';
