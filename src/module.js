022013b3172775c7550c740564eefd5e265ce396
Christoph Guttandin <chrisguttandin@media-codings.com>
2016-02-22 15:02:40 +0100

    export OfflineAudioContext

--- src/module.js
+++ src/module.js
@@ -7,7 +7,9 @@ import { PromiseSupportTester } from './tester/promise-support';
 import { audioContextConstructor } from './audio-context-constructor';
 import { isSupportedFlag } from './is-supported-flag';
 import { modernizr } from './modernizr';
+import { offlineAudioContextConstructor } from './offline-audio-context-constructor';
 import { unpatchedAudioContextConstructor } from './unpatched-audio-context-constructor';
+import { unpatchedOfflineAudioContextConstructor } from './unpatched-offline-audio-context-constructor';
 import { window } from './window.js';
 
 /* eslint-disable indent, new-cap */
@@ -19,7 +21,9 @@ var injector = Injector.resolveAndCreate([
         provide(audioContextConstructor, { useFactory: audioContextConstructor }),
         provide(isSupportedFlag, { useFactory: isSupportedFlag }),
         provide(modernizr, { useValue: modernizr }),
+        provide(offlineAudioContextConstructor, { useFactory: offlineAudioContextConstructor }),
         provide(unpatchedAudioContextConstructor, { useFactory: unpatchedAudioContextConstructor }),
+        provide(unpatchedOfflineAudioContextConstructor, { useFactory: unpatchedOfflineAudioContextConstructor }),
         provide(window, { useValue: window })
     ]);
 /* eslint-enable indent, new-cap */
@@ -27,3 +31,5 @@ var injector = Injector.resolveAndCreate([
 export const AudioContext = injector.get(audioContextConstructor);
 
 export const isSupported = injector.get(isSupportedFlag);
+
+export const OfflineAudioContext = injector.get(offlineAudioContextConstructor);
