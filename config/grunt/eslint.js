69969f319f7c374876895f420e739db6451426d2
chrisguttandin <chrisguttandin@media-codings.com>
2016-06-25 16:27:00 +0200

    remove obsolete parser options

--- config/grunt/eslint.js
+++ config/grunt/eslint.js
@@ -3,10 +3,7 @@
 module.exports = {
     src: {
         options: {
-            configFile: 'config/eslint/src.json',
-            parserOptions: {
-                sourceType: 'module'
-            }
+            configFile: 'config/eslint/src.json'
         },
         src: [
             'src/**/*.js'
