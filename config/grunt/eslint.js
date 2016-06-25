ce215096b4c56d610da16fd50acab569c8b34981
chrisguttandin <chrisguttandin@media-codings.com>
2016-06-25 16:48:25 +0200

    reorder eslint config

--- config/grunt/eslint.js
+++ config/grunt/eslint.js
@@ -1,14 +1,6 @@
 'use strict';
 
 module.exports = {
-    src: {
-        options: {
-            configFile: 'config/eslint/src.json'
-        },
-        src: [
-            'src/**/*.js'
-        ]
-    },
     config: {
         options: {
             configFile: 'config/eslint/config.json'
@@ -16,6 +8,14 @@ module.exports = {
         src: [
             '*.js',
             'config/**/*.js'
+        ]
+    },
+    src: {
+        options: {
+            configFile: 'config/eslint/src.json'
+        },
+        src: [
+            'src/**/*.js'
         ]
     },
     test: {
