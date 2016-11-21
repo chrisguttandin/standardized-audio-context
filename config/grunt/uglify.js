9db1ad3d4a00ded0b7e4eafaf86fe0d8352b33ed
chrisguttandin <chrisguttandin@media-codings.com>
2016-11-21 23:24:17 +0100

    change minified file

--- config/grunt/uglify.js
+++ config/grunt/uglify.js
@@ -1,8 +1,8 @@
 module.exports = {
     bundle: {
         files: {
-            'build/es5/bundle.min.js': [
-                'build/es5/bundle.js'
+            'build/es5/script.min.js': [
+                'build/es5/script.js'
             ]
         }
     }
---
+++ config/rollup/script.js
@@ -0,0 +1,27 @@
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+
+export default {
+    dest: 'build/es5/script.js',
+    entry: 'build/es2015/module.js',
+    format: 'umd',
+    moduleName: 'standardizedAudioContext',
+    plugins: [
+        babel({
+            presets: [
+                [
+                    'es2015',
+                    {
+                        modules: false
+                    }
+                ]
+            ]
+        }),
+        commonjs(),
+        nodeResolve({
+            jsnext: true,
+            main: true
+        })
+    ]
+};
