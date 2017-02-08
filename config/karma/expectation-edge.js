7e1b6e8d96d292cb215f6b6318d271faaf04a32b
chrisguttandin <chrisguttandin@media-codings.com>
2017-02-08 14:44:45 +0100

    add captureTimeout to Edge test config

--- config/karma/expectation-edge.js
+++ config/karma/expectation-edge.js
@@ -45,7 +45,10 @@ module.exports = (config) => {
             browserStack: {
                 accessKey: process.env.BROWSER_STACK_ACCESS_KEY,
                 username: process.env.BROWSER_STACK_USERNAME
-            }
+            },
+
+            captureTimeout: 120000
+
         });
 
     } else {
