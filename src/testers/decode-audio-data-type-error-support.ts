05d1f4fda452e0e8b29e69ce9727059dca097a22
chrisguttandin <chrisguttandin@media-codings.com>
2017-04-27 14:22:50 +0200

    catch promise errors as well

--- src/testers/decode-audio-data-type-error-support.ts
+++ src/testers/decode-audio-data-type-error-support.ts
@@ -31,6 +31,9 @@ export class DecodeAudioDataTypeErrorSupportTester {
                         });
 
                     resolve(err instanceof TypeError);
+                })
+                .catch(() => {
+                    // Ignore errors.
                 });
         });
     }
