fcc910fe3e57ef08bb536544f201f0e863f7af5d
chrisguttandin <chrisguttandin@media-codings.com>
2017-05-05 15:02:14 +0200

    refine repalce:modernizr task

--- config/grunt/replace.js
+++ config/grunt/replace.js
@@ -13,13 +13,40 @@ module.exports = {
 }
 declare var window: WindowWithPromise;
 
-export default (function(window: any, document: any, undefined?: any){`
+export default (function(window: any){`
+            }, {
+                match: /var\stests\s=\s\[\];/g,
+                replacement: 'var tests: any[] = [];'
+            }, {
+                match: /_q:\s\[\],/g,
+                replacement: '_q: <any[]> [],'
+            }, {
+                match: /on:\sfunction\(test,\scb\)\s{/g,
+                replacement: 'on: function(this: any, test: any, cb: any) {'
+            }, {
+                match: /addTest:\sfunction\(name,\sfn,\soptions\)\s{/g,
+                replacement: 'addTest: function(name: any, fn: any, options: any) {'
+            }, {
+                match: /addAsyncTest:\sfunction\(fn\)\s{/g,
+                replacement: 'addAsyncTest: function(fn: any) {'
             }, {
                 match: /var\sModernizr\s=\sfunction\(\)\s\{};/g,
                 replacement: 'var Modernizr:any = function() {};'
             }, {
+                match: /var\sclasses\s=\s\[\];/g,
+                replacement: 'var classes: any[] = [];'
+            }, {
+                match: /function\sis\(obj,\stype\)\s\{/g,
+                replacement: 'function is(obj: any, type: any) {'
+            }, {
                 match: /window.Modernizr\s=\s(Modernizr;)/g,
                 replacement: 'return $1'
+            }, {
+                match: /\}\)\(window,\sdocument\);/g,
+                replacement: '})(window);'
+            }, {
+                match: /new\swindow.Promise\(function\(r\)\s\{\sresolve\s=\sr;\s\}\);/g,
+                replacement: 'new window.Promise(function(r: any) { resolve = r; });'
             } ]
         }
     }
