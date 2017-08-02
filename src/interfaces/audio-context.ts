bdc481cc35577829f71a3a4976c901afa6822156
chrisguttandin <chrisguttandin@media-codings.com>
2017-08-02 14:42:55 +0200

    add createOscillator() as a method of IAudioContext

--- src/interfaces/audio-context.ts
+++ src/interfaces/audio-context.ts
@@ -1,5 +1,6 @@
 import { IAnalyserNode } from './analyser-node';
 import { IBaseAudioContext } from './base-audio-context';
+import { IOscillatorNode } from './oscillator-node';
 
 export interface IAudioContext extends IBaseAudioContext {
 
@@ -13,5 +14,8 @@ export interface IAudioContext extends IBaseAudioContext {
 
     // @todo This should move into the IBaseAudioContext interface.
     createChannelSplitter(numberOfOutputs: number): ChannelSplitterNode;
+
+    // @todo This should move into the IBaseAudioContext interface.
+    createOscillator(): IOscillatorNode;
 
 }
