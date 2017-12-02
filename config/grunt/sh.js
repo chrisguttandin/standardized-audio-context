516fa976234048246c3bbaf37e5b76d0ca993495
chrisguttandin <chrisguttandin@media-codings.com>
2017-12-02 02:12:32 +0100

    build(config): update grunt config

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -9,6 +9,6 @@ module.exports = {
         cmd: 'tsc -p src/tsconfig.json --declaration false --target es5 --outDir build/esm'
     },
     'lint': {
-        cmd: 'tslint -c config/tslint/src.json --project src/tsconfig.json src/**/*.ts'
+        cmd: 'tslint --config config/tslint/src.json --exclude src/browsernizr.ts --project src/tsconfig.json src/*.ts src/**/*.ts'
     }
 };
