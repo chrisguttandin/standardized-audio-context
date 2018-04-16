8f909b3a5796ffc7865188d1998e6d050116e8c3
chrisguttandin <chrisguttandin@media-codings.com>
2018-04-16 14:56:12 +0200

    fix: only assign changed AudioNode options

--- src/helpers/assign-native-audio-node-options.ts
+++ src/helpers/assign-native-audio-node-options.ts
@@ -1,16 +1,21 @@
 import { IAudioNodeOptions } from '../interfaces';
 import { TNativeAudioNode } from '../types';
 
+// @todo Use the same strategy to assign all node specific options as well.
+const assignNativeAudioNodeOption = (
+    nativeAudioNode: TNativeAudioNode,
+    options: Partial<IAudioNodeOptions>,
+    option: 'channelCount' | 'channelCountMode' | 'channelInterpretation'
+) => {
+    const value = options[option];
+
+    if (value !== undefined && value !== nativeAudioNode[option]) {
+        nativeAudioNode[option] = value;
+    }
+};
+
 export const assignNativeAudioNodeOptions = (nativeAudioNode: TNativeAudioNode, options: Partial<IAudioNodeOptions> = { }): void => {
-    if (options.channelCount !== undefined) {
-        nativeAudioNode.channelCount = options.channelCount;
-    }
-
-    if (options.channelCountMode !== undefined) {
-        nativeAudioNode.channelCountMode = options.channelCountMode;
-    }
-
-    if (options.channelInterpretation !== undefined) {
-        nativeAudioNode.channelInterpretation = options.channelInterpretation;
-    }
+    assignNativeAudioNodeOption(nativeAudioNode, options, 'channelCount');
+    assignNativeAudioNodeOption(nativeAudioNode, options, 'channelCountMode');
+    assignNativeAudioNodeOption(nativeAudioNode, options, 'channelInterpretation');
 };
