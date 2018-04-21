9a1a9ca1a870f7529573ce8d641c4a6471bdac0a
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-04-22 01:48:09 +0200

    fix: delete processor definitions

--- src/helpers/delete-audio-graph.ts
+++ src/helpers/delete-audio-graph.ts
@@ -1,4 +1,4 @@
-import { AUDIO_GRAPHS, CONTEXT_STORE } from '../globals';
+import { AUDIO_GRAPHS, CONTEXT_STORE, NODE_NAME_TO_PROCESSOR_DEFINITION_MAPS, NODE_TO_PROCESSOR_MAPS } from '../globals';
 import { TContext, TNativeContext } from '../types';
 import { disconnectAudioNodeInputConnections } from './disconnect-audio-node-input-connections';
 
@@ -13,4 +13,8 @@ export const deleteAudioGraph = (context: TContext, nativeContext: TNativeContex
     AUDIO_GRAPHS.delete(nativeContext);
 
     CONTEXT_STORE.delete(context);
+
+    NODE_NAME_TO_PROCESSOR_DEFINITION_MAPS.delete(nativeContext);
+
+    NODE_TO_PROCESSOR_MAPS.delete(nativeContext);
 };
