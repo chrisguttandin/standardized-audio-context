7360d60b98e21c2ce2559929b88319ef8aa67e2d
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-06-24 12:05:32 +0200

    fix: remove unnecessary accessors

--- src/factories/none-audio-destination-node-constructor.ts
+++ src/factories/none-audio-destination-node-constructor.ts
@@ -8,8 +8,6 @@ export const createNoneAudioDestinationNodeConstructor: TNoneAudioDestinationNod
 
     return class NoneAudioDestinationNode extends audioNodeConstructor {
 
-        private _nativeAudioNode: TNativeAudioNode;
-
         constructor (context: TContext, nativeAudioNode: TNativeAudioNode, audioNodeRenderer: null | IAudioNodeRenderer) {
             // Bug #50 Safari does not throw an error when the context is already closed.
             if (context.state === 'closed') {
@@ -17,24 +15,6 @@ export const createNoneAudioDestinationNodeConstructor: TNoneAudioDestinationNod
             }
 
             super(context, nativeAudioNode, audioNodeRenderer);
-
-            this._nativeAudioNode = nativeAudioNode;
-        }
-
-        public get channelCount () {
-            return this._nativeAudioNode.channelCount;
-        }
-
-        public set channelCount (value) {
-            this._nativeAudioNode.channelCount = value;
-        }
-
-        public get channelCountMode () {
-            return this._nativeAudioNode.channelCountMode;
-        }
-
-        public set channelCountMode (value) {
-            this._nativeAudioNode.channelCountMode = value;
         }
 
     };
