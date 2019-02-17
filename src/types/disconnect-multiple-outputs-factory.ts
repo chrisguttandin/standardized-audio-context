a260f449729ba42540437371542e2ea0ca5f42bb
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-02-17 01:35:23 +0100

    style: condense type declaration to one line

--- src/types/disconnect-multiple-outputs-factory.ts
+++ src/types/disconnect-multiple-outputs-factory.ts
@@ -1,6 +1,4 @@
 import { TDisconnectMultipleOutputsFunction } from './disconnect-multiple-outputs-function';
 import { TIndexSizeErrorFactory } from './index-size-error-factory';
 
-export type TDisconnectMultipleOutputsFactory = (
-    createIndexSizeError: TIndexSizeErrorFactory
-) => TDisconnectMultipleOutputsFunction;
+export type TDisconnectMultipleOutputsFactory = (createIndexSizeError: TIndexSizeErrorFactory) => TDisconnectMultipleOutputsFunction;
