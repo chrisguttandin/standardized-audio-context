08332caefe13269ed332ccfd52a7435761291494
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-06-01 13:43:47 +0200

    refactor: use TypeScript's built-in Omit type

--- src/types/native-panner-node.ts
+++ src/types/native-panner-node.ts
@@ -1,2 +1,2 @@
-// This is removing the deprecated functions setOrientation() and setPosition() from the native PannerNode type.
-export type TNativePannerNode = Pick<PannerNode, Exclude<keyof PannerNode, 'setOrientation' | 'setPosition'>>;
+// @todo This is removing the deprecated functions setOrientation() and setPosition() from the native PannerNode type.
+export type TNativePannerNode = Omit<PannerNode, 'setOrientation' | 'setPosition'>;
