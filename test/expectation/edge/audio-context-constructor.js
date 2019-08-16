37507da9ab9d375515aee659610b80a5201ad41e
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-08-17 00:16:34 +0200

    test: remove unrunnable expectation test for Edge

--- test/expectation/edge/audio-context-constructor.js
+++ test/expectation/edge/audio-context-constructor.js
@@ -696,26 +696,6 @@ describe('audioContextConstructor', () => {
 
         });
 
-        describe('createMediaStreamSource()', () => {
-
-            describe('mediaStream', () => {
-
-                let mediaStreamAudioSourceNode;
-
-                beforeEach(() => {
-                    mediaStreamAudioSourceNode = audioContext.createMediaStreamSource(audioContext.createMediaStreamDestination().stream);
-                });
-
-                // bug #63
-
-                it('should not be implemented', () => {
-                    expect(mediaStreamAudioSourceNode.mediaStream).to.be.undefined;
-                });
-
-            });
-
-        });
-
         describe('createMediaStreamDestination()', () => {
 
             // bug #64
