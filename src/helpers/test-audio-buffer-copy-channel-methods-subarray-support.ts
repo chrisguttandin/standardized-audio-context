d348a8f250dacd3e870c79a7e3d617d8809e0c7d
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-08-19 23:08:27 +0200

    fix: use copyFromChannel() to check browser support
    fix #645

--- src/helpers/test-audio-buffer-copy-channel-methods-subarray-support.ts
+++ src/helpers/test-audio-buffer-copy-channel-methods-subarray-support.ts
@@ -8,7 +8,7 @@ export const testAudioBufferCopyChannelMethodsSubarraySupport = (nativeAudioBuff
          * Firefox up to version 67 didn't fully support the copyFromChannel() and copyToChannel() methods. Therefore testing one of those
          * methods is enough to know if the other one it supported as well.
          */
-        nativeAudioBuffer.copyToChannel(source, 0, nativeAudioBuffer.length - 1);
+        nativeAudioBuffer.copyFromChannel(source, 0, nativeAudioBuffer.length - 1);
     } catch {
         return false;
     }
