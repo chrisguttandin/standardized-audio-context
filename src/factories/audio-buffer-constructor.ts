75668670e3ea65ec000bbb10eb3e1b2b8d7e86cf
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-09-04 11:12:18 +0200

    fix: use custom instanceof check to avoid endless recursion

--- src/factories/audio-buffer-constructor.ts
+++ src/factories/audio-buffer-constructor.ts
@@ -102,7 +102,7 @@ export const createAudioBufferConstructor: TAudioBufferConstructorFactory = (
         }
 
         public static [ Symbol.hasInstance ] (instance: unknown): boolean {
-            return (instance instanceof AudioBuffer) || (audioBufferStore.has(<any> instance));
+            return (Object.getPrototypeOf(instance) === AudioBuffer.prototype) || (audioBufferStore.has(<any> instance));
         }
 
     };
--- src/factories/periodic-wave-constructor.ts
+++ src/factories/periodic-wave-constructor.ts
@@ -26,7 +26,7 @@ export const createPeriodicWaveConstructor: TPeriodicWaveConstructorFactory = (c
         }
 
         public static [ Symbol.hasInstance ] (instance: unknown): boolean {
-            return (instance instanceof PeriodicWave) || (periodicWaveStore.has(<any> instance));
+            return (Object.getPrototypeOf(instance) === PeriodicWave.prototype) || (periodicWaveStore.has(<any> instance));
         }
 
     };
