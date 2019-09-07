fc87c6eb7a341424f0438528896da5b21794ab30
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-09-08 00:32:23 +0200

    test: update expectation tests for Firefox v69

--- test/expectation/firefox/current/offline-audio-context-constructor.js
+++ test/expectation/firefox/current/offline-audio-context-constructor.js
@@ -6,26 +6,6 @@ describe('offlineAudioContextConstructor', () => {
         offlineAudioContext = new OfflineAudioContext(1, 256000, 44100);
     });
 
-    describe('createBiquadFilter()', () => {
-
-        let biquadFilterNode;
-
-        beforeEach(() => {
-            biquadFilterNode = offlineAudioContext.createBiquadFilter();
-        });
-
-        describe('getFrequencyResponse()', () => {
-
-            // bug #68
-
-            it('should throw no error', () => {
-                biquadFilterNode.getFrequencyResponse(new Float32Array(), new Float32Array(1), new Float32Array(1));
-            });
-
-        });
-
-    });
-
     describe('createChannelMerger()', () => {
 
         // bug #16
--- test/expectation/firefox/legacy/audio-context-constructor.js
+++ test/expectation/firefox/legacy/audio-context-constructor.js
@@ -28,26 +28,6 @@ describe('audioContextConstructor', () => {
 
     });
 
-    describe('close()', () => {
-
-        // bug #50
-
-        it('should not allow to create AudioNodes on a closed context', (done) => {
-            audioContext
-                .close()
-                .then(() => {
-                    audioContext.createGain();
-                })
-                .catch(() => {
-                    // Create a closeable AudioContext to align the behaviour with other tests.
-                    audioContext = new AudioContext();
-
-                    done();
-                });
-        });
-
-    });
-
     describe('createBiquadFilter()', () => {
 
         let biquadFilterNode;
@@ -68,38 +48,6 @@ describe('audioContextConstructor', () => {
 
     });
 
-    describe('createBuffer()', () => {
-
-        // bug #42
-
-        describe('copyFromChannel()/copyToChannel()', () => {
-
-            let audioBuffer;
-
-            beforeEach(() => {
-                audioBuffer = audioContext.createBuffer(2, 100, 44100);
-            });
-
-            it('should not allow to copy only a part to the source', () => {
-                const source = new Float32Array(10);
-
-                expect(() => {
-                    audioBuffer.copyToChannel(source, 0, 95);
-                }).to.throw(Error);
-            });
-
-            it('should not allow to copy only a part of the destination', () => {
-                const destination = new Float32Array(10);
-
-                expect(() => {
-                    audioBuffer.copyFromChannel(destination, 0, 95);
-                }).to.throw(Error);
-            });
-
-        });
-
-    });
-
     describe('createChannelMerger()', () => {
 
         // bug #16
@@ -108,36 +56,6 @@ describe('audioContextConstructor', () => {
             const channelMergerNode = audioContext.createChannelMerger();
 
             channelMergerNode.channelCountMode = 'max';
-        });
-
-    });
-
-    describe('createDynamicsCompressor()', () => {
-
-        let dynamicsCompressorNode;
-
-        beforeEach(() => {
-            dynamicsCompressorNode = audioContext.createDynamicsCompressor();
-        });
-
-        describe('channelCount', () => {
-
-            // bug #108
-
-            it('should not throw an error', () => {
-                dynamicsCompressorNode.channelCount = 3;
-            });
-
-        });
-
-        describe('channelCountMode', () => {
-
-            // bug #109
-
-            it('should not throw an error', () => {
-                dynamicsCompressorNode.channelCountMode = 'max';
-            });
-
         });
 
     });
@@ -351,16 +269,6 @@ describe('audioContextConstructor', () => {
                 }, 1000);
             });
 
-        });
-
-    });
-
-    describe('createMediaStreamTrackSource()', () => {
-
-        // bug #121
-
-        it('should not be implemented', () => {
-            expect(audioContext.createMediaStreamTrackSource).to.be.undefined;
         });
 
     });
