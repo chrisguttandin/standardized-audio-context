cdd1a2c8cc1d0d6258f0a360c90cf94de3b65244
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-09-25 09:45:19 +0200

    test: define run functions in beforeEach block

--- test/memory/module.js
+++ test/memory/module.js
@@ -73,131 +73,182 @@ describe('module', () => {
 
     describe('with a GainNode', () => {
 
-        it('should collect unconnected GainNodes', async function () {
-            this.timeout(10000);
+        describe('with unconnected GainNodes', () => {
 
-            const run = (numberOfIterations) => {
-                for (let i = 0; i < numberOfIterations; i += 1) {
-                    new GainNode(audioContext); // eslint-disable-line no-undef
-                }
-            };
-            const numberOfObjects = await countObjects(page);
+            let run;
 
-            await page.evaluate(run, 1000);
+            beforeEach(() => {
+                run = (numberOfIterations) => {
+                    for (let i = 0; i < numberOfIterations; i += 1) {
+                        new GainNode(audioContext); // eslint-disable-line no-undef
+                    }
+                };
+            });
 
-            expect(await countObjects(page)).to.equal(numberOfObjects);
+            it('should collect all GainNodes', async function () {
+                this.timeout(10000);
+
+                const numberOfObjects = await countObjects(page);
+
+                await page.evaluate(run, 1000);
+
+                expect(await countObjects(page)).to.equal(numberOfObjects);
+            });
+
         });
 
-        it('should collect connected GainNodes', async function () {
-            this.timeout(10000);
+        describe('with connected GainNodes', () => {
 
-            const run = (numberOfIterations) => {
-                for (let i = 0; i < numberOfIterations; i += 1) {
-                    const gainNode = new GainNode(audioContext); // eslint-disable-line no-undef
+            let run;
 
-                    gainNode.connect(audioContext.destination); // eslint-disable-line no-undef
-                }
-            };
-            const numberOfObjects = await countObjects(page);
+            beforeEach(() => {
+                run = (numberOfIterations) => {
+                    for (let i = 0; i < numberOfIterations; i += 1) {
+                        const gainNode = new GainNode(audioContext); // eslint-disable-line no-undef
 
-            await page.evaluate(run, 1000);
+                        gainNode.connect(audioContext.destination); // eslint-disable-line no-undef
+                    }
+                };
+            });
 
-            expect(await countObjects(page)).to.equal(numberOfObjects);
+            it('should collect all GainNodes', async function () {
+                this.timeout(10000);
+
+                const numberOfObjects = await countObjects(page);
+
+                await page.evaluate(run, 1000);
+
+                expect(await countObjects(page)).to.equal(numberOfObjects);
+            });
+
         });
 
-        it('should collect disconnected GainNodes', async function () {
-            this.timeout(10000);
+        describe('with disconnected GainNodes', () => {
 
-            const run = (numberOfIterations) => {
-                for (let i = 0; i < numberOfIterations; i += 1) {
-                    const gainNode = new GainNode(audioContext); // eslint-disable-line no-undef
+            let run;
 
-                    gainNode.connect(audioContext.destination); // eslint-disable-line no-undef
-                    gainNode.disconnect(audioContext.destination); // eslint-disable-line no-undef
-                }
-            };
-            const numberOfObjects = await countObjects(page);
+            beforeEach(() => {
+                run = (numberOfIterations) => {
+                    for (let i = 0; i < numberOfIterations; i += 1) {
+                        const gainNode = new GainNode(audioContext); // eslint-disable-line no-undef
 
-            await page.evaluate(run, 1000);
+                        gainNode.connect(audioContext.destination); // eslint-disable-line no-undef
+                        gainNode.disconnect(audioContext.destination); // eslint-disable-line no-undef
+                    }
+                };
+            });
 
-            expect(await countObjects(page)).to.equal(numberOfObjects);
+            it('should collect all GainNodes', async function () {
+                this.timeout(10000);
+
+                const numberOfObjects = await countObjects(page);
+
+                await page.evaluate(run, 1000);
+
+                expect(await countObjects(page)).to.equal(numberOfObjects);
+            });
+
         });
 
     });
 
     describe('with an AudioBufferSourceNode', () => {
 
-        it('should collect unconnected AudioBufferSourceNodes', async function () {
-            this.timeout(10000);
+        describe('with unconnected AudioBufferSourceNodes', () => {
 
-            const run = (numberOfIterations) => {
-                for (let i = 0; i < numberOfIterations; i += 1) {
-                    new AudioBufferSourceNode(
-                        audioContext, // eslint-disable-line no-undef
-                        { buffer: new AudioBuffer({ length: 1, sampleRate: audioContext.sampleRate }) } // eslint-disable-line no-undef
-                    );
-                }
-            };
+            let run;
 
-            // Run the test once because the first run will trigger some memoizations.
-            await page.evaluate(run, 1);
+            beforeEach(() => {
+                run = (numberOfIterations) => {
+                    for (let i = 0; i < numberOfIterations; i += 1) {
+                        new AudioBufferSourceNode(
+                            audioContext, // eslint-disable-line no-undef
+                            { buffer: new AudioBuffer({ length: 1, sampleRate: audioContext.sampleRate }) } // eslint-disable-line no-undef
+                        );
+                    }
+                };
+            });
 
-            const numberOfObjects = await countObjects(page);
+            it('should collect all AudioBufferSourceNodes', async function () {
+                this.timeout(10000);
 
-            await page.evaluate(run, 1000);
+                // Run the test once because the first run will trigger some memoizations.
+                await page.evaluate(run, 1);
 
-            expect(await countObjects(page)).to.equal(numberOfObjects);
+                const numberOfObjects = await countObjects(page);
+
+                await page.evaluate(run, 1000);
+
+                expect(await countObjects(page)).to.equal(numberOfObjects);
+            });
+
         });
 
-        it('should collect connected AudioBufferSourceNodes', async function () {
-            this.timeout(10000);
+        describe('with connected AudioBufferSourceNodes', () => {
 
-            const run = (numberOfIterations) => {
-                for (let i = 0; i < numberOfIterations; i += 1) {
-                    const audioBufferSourceNode = new AudioBufferSourceNode(
-                        audioContext, // eslint-disable-line no-undef
-                        { buffer: new AudioBuffer({ length: 1, sampleRate: audioContext.sampleRate }) } // eslint-disable-line no-undef
-                    );
+            let run;
 
-                    audioBufferSourceNode.connect(audioContext.destination); // eslint-disable-line no-undef
-                }
-            };
+            beforeEach(() => {
+                run = (numberOfIterations) => {
+                    for (let i = 0; i < numberOfIterations; i += 1) {
+                        const audioBufferSourceNode = new AudioBufferSourceNode(
+                            audioContext, // eslint-disable-line no-undef
+                            { buffer: new AudioBuffer({ length: 1, sampleRate: audioContext.sampleRate }) } // eslint-disable-line no-undef
+                        );
 
-            // Run the test once because the first run will trigger some memoizations.
-            await page.evaluate(run, 1);
+                        audioBufferSourceNode.connect(audioContext.destination); // eslint-disable-line no-undef
+                    }
+                };
+            });
 
-            const numberOfObjects = await countObjects(page);
+            it('should collect all AudioBufferSourceNodes', async function () {
+                this.timeout(10000);
 
-            await page.evaluate(run, 1000);
+                // Run the test once because the first run will trigger some memoizations.
+                await page.evaluate(run, 1);
 
-            expect(await countObjects(page)).to.equal(numberOfObjects);
+                const numberOfObjects = await countObjects(page);
+
+                await page.evaluate(run, 1000);
+
+                expect(await countObjects(page)).to.equal(numberOfObjects);
+            });
+
         });
 
         // @todo Run a test with started AudioBufferSourceNodes.
 
-        it('should collect disconnected AudioBufferSourceNodes', async function () {
-            this.timeout(10000);
+        describe('with disconnected AudioBufferSourceNodes', () => {
 
-            const run = (numberOfIterations) => {
-                for (let i = 0; i < numberOfIterations; i += 1) {
-                    const audioBufferSourceNode = new AudioBufferSourceNode(
-                        audioContext, // eslint-disable-line no-undef
-                        { buffer: new AudioBuffer({ length: 1, sampleRate: audioContext.sampleRate }) } // eslint-disable-line no-undef
-                    );
+            let run;
 
-                    audioBufferSourceNode.connect(audioContext.destination); // eslint-disable-line no-undef
-                    audioBufferSourceNode.disconnect(audioContext.destination); // eslint-disable-line no-undef
-                }
-            };
+            beforeEach(() => {
+                run = (numberOfIterations) => {
+                    for (let i = 0; i < numberOfIterations; i += 1) {
+                        const audioBufferSourceNode = new AudioBufferSourceNode(
+                            audioContext, // eslint-disable-line no-undef
+                            { buffer: new AudioBuffer({ length: 1, sampleRate: audioContext.sampleRate }) } // eslint-disable-line no-undef
+                        );
 
-            // Run the test once because the first run will trigger some memoizations.
-            await page.evaluate(run, 1);
+                        audioBufferSourceNode.connect(audioContext.destination); // eslint-disable-line no-undef
+                        audioBufferSourceNode.disconnect(audioContext.destination); // eslint-disable-line no-undef
+                    }
+                };
+            });
 
-            const numberOfObjects = await countObjects(page);
+            it('should collect all AudioBufferSourceNodes', async function () {
+                this.timeout(10000);
 
-            await page.evaluate(run, 1000);
+                // Run the test once because the first run will trigger some memoizations.
+                await page.evaluate(run, 1);
 
-            expect(await countObjects(page)).to.equal(numberOfObjects);
+                const numberOfObjects = await countObjects(page);
+
+                await page.evaluate(run, 1000);
+
+                expect(await countObjects(page)).to.equal(numberOfObjects);
+            });
+
         });
 
     });
