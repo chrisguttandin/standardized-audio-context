66f87cbf9b14551ad9416854d6fe420aa708182b
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-03-17 00:43:03 +0100

    test: update expectation tests for Firefox v74

--- test/expectation/firefox/current/offline-audio-context-constructor.js
+++ test/expectation/firefox/current/offline-audio-context-constructor.js
@@ -6,23 +6,6 @@ describe('offlineAudioContextConstructor', () => {
         offlineAudioContext = new OfflineAudioContext(1, 256000, 44100);
     });
 
-    describe('createBufferSource()', () => {
-
-        describe('buffer', () => {
-
-            // bug #72
-
-            it('should allow to assign the buffer multiple times', () => {
-                const audioBufferSourceNode = offlineAudioContext.createBufferSource();
-
-                audioBufferSourceNode.buffer = offlineAudioContext.createBuffer(2, 100, 44100);
-                audioBufferSourceNode.buffer = offlineAudioContext.createBuffer(2, 100, 44100);
-            });
-
-        });
-
-    });
-
     describe('createOscillator()', () => {
 
         let oscillatorNode;
