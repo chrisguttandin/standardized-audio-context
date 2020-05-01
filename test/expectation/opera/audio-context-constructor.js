6854b80dd15f690a3d4bf3b056b2c8f4b8cdb67e
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-05-01 11:52:04 +0200

    test: update expectation tests for Opera v68

--- test/expectation/opera/audio-context-constructor.js
+++ test/expectation/opera/audio-context-constructor.js
@@ -146,54 +146,6 @@ describe('audioContextConstructor', () => {
 
     });
 
-    describe('createBiquadFilter()', () => {
-
-        let biquadFilterNode;
-
-        beforeEach(() => {
-            biquadFilterNode = audioContext.createBiquadFilter();
-        });
-
-        describe('detune', () => {
-
-            describe('maxValue', () => {
-
-                // bug #78
-
-                it('should be the largest possible positive float value', () => {
-                    expect(biquadFilterNode.detune.maxValue).to.equal(3.4028234663852886e+38);
-                });
-
-            });
-
-            describe('minValue', () => {
-
-                // bug #78
-
-                it('should be the smallest possible negative float value', () => {
-                    expect(biquadFilterNode.detune.minValue).to.equal(-3.4028234663852886e+38);
-                });
-
-            });
-
-        });
-
-        describe('gain', () => {
-
-            describe('maxValue', () => {
-
-                // bug #79
-
-                it('should be the largest possible positive float value', () => {
-                    expect(biquadFilterNode.gain.maxValue).to.equal(3.4028234663852886e+38);
-                });
-
-            });
-
-        });
-
-    });
-
     describe('createBufferSource()', () => {
 
         describe('stop()', () => {
@@ -204,40 +156,6 @@ describe('audioContextConstructor', () => {
                 const audioBufferSourceNode = audioContext.createBufferSource();
 
                 expect(() => audioBufferSourceNode.stop(-1)).to.throw(DOMException).with.property('name', 'InvalidStateError');
-            });
-
-        });
-
-    });
-
-    describe('createConvolver()', () => {
-
-        let convolverNode;
-
-        beforeEach(() => {
-            convolverNode = audioContext.createConvolver();
-        });
-
-        describe('channelCount', () => {
-
-            // bug #166
-
-            it('should throw an error', () => {
-                expect(() => {
-                    convolverNode.channelCount = 1;
-                }).to.throw(DOMException);
-            });
-
-        });
-
-        describe('channelCountMode', () => {
-
-            // bug #167
-
-            it('should throw an error', () => {
-                expect(() => {
-                    convolverNode.channelCountMode = 'explicit';
-                }).to.throw(DOMException);
             });
 
         });
