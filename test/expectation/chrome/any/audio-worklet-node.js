8ba93f5e9195e45d50ce52ddf4d3f16b9bc1349d
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-05-01 11:50:04 +0200

    test: use OAC to test AudioWorklet implementation in Chrome and Opera

--- test/expectation/chrome/any/audio-worklet-node.js
+++ test/expectation/chrome/any/audio-worklet-node.js
@@ -2,12 +2,10 @@ import { spy } from 'sinon';
 
 describe('AudioWorklet', () => {
 
-    let audioContext;
-
-    afterEach(() => audioContext.close());
+    let offlineAudioContext;
 
     beforeEach(() => {
-        audioContext = new AudioContext();
+        offlineAudioContext = new OfflineAudioContext(1, 256000, 44100);
     });
 
     describe('with the name of an unknown processor', () => {
@@ -16,7 +14,7 @@ describe('AudioWorklet', () => {
 
         it('should throw an InvalidStateError', (done) => {
             try {
-                new AudioWorkletNode(audioContext, 'unknown-processor');
+                new AudioWorkletNode(offlineAudioContext, 'unknown-processor');
             } catch (err) {
                 expect(err.code).to.equal(11);
                 expect(err.name).to.equal('InvalidStateError');
@@ -34,9 +32,9 @@ describe('AudioWorklet', () => {
         it('should be 3.402820018375656e+38 and -3.402820018375656e+38', async function () {
             this.timeout(10000);
 
-            await audioContext.audioWorklet.addModule('base/test/fixtures/gain-processor.js');
+            await offlineAudioContext.audioWorklet.addModule('base/test/fixtures/gain-processor.js');
 
-            const audioWorkletNode = new AudioWorkletNode(audioContext, 'gain-processor');
+            const audioWorkletNode = new AudioWorkletNode(offlineAudioContext, 'gain-processor');
 
             expect(audioWorkletNode.parameters.get('gain').maxValue).to.equal(3.402820018375656e+38);
             expect(audioWorkletNode.parameters.get('gain').minValue).to.equal(-3.402820018375656e+38);
@@ -44,39 +42,35 @@ describe('AudioWorklet', () => {
 
     });
 
-    // @todo For some reason this test does currently not pass when running on BrowserStack.
-    if (!process.env.TRAVIS) { // eslint-disable-line no-undef
+    describe('without any connected inputs', () => {
 
-        describe('without any connected inputs', () => {
+        // bug #170
 
-            // bug #170
+        it('should call process() with an array with empty channelData for each input', function (done) {
+            this.timeout(10000);
 
-            it('should call process() with an array with empty channelData for each input', function (done) {
-                this.timeout(10000);
+            offlineAudioContext.audioWorklet
+                .addModule('base/test/fixtures/inspector-processor.js')
+                .then(() => {
+                    const audioWorkletNode = new AudioWorkletNode(offlineAudioContext, 'inspector-processor');
 
-                audioContext.audioWorklet
-                    .addModule('base/test/fixtures/inspector-processor.js')
-                    .then(() => {
-                        const audioWorkletNode = new AudioWorkletNode(audioContext, 'inspector-processor');
+                    audioWorkletNode.connect(offlineAudioContext.destination);
 
-                        audioWorkletNode.connect(audioContext.destination);
+                    audioWorkletNode.port.onmessage = ({ data }) => {
+                        audioWorkletNode.port.onmessage = null;
 
-                        audioWorkletNode.port.onmessage = ({ data }) => {
-                            audioWorkletNode.port.onmessage = null;
+                        expect(data.inputs.length).to.equal(1);
+                        expect(data.inputs[0].length).to.equal(1);
+                        expect(data.inputs[0][0].length).to.equal(0);
 
-                            expect(data.inputs.length).to.equal(1);
-                            expect(data.inputs[0].length).to.equal(1);
-                            expect(data.inputs[0][0].length).to.equal(0);
+                        done();
+                    };
 
-                            done();
-                        };
-                    });
-            });
-
-
+                    offlineAudioContext.startRendering();
+                });
         });
 
-    }
+    });
 
     describe('without any connected outputs', () => {
 
@@ -85,11 +79,11 @@ describe('AudioWorklet', () => {
         it('should not call process()', function (done) {
             this.timeout(10000);
 
-            audioContext.audioWorklet
+            offlineAudioContext.audioWorklet
                 .addModule('base/test/fixtures/inspector-processor.js')
                 .then(() => {
-                    const audioWorkletNode = new AudioWorkletNode(audioContext, 'inspector-processor');
-                    const constantSourceNode = new ConstantSourceNode(audioContext);
+                    const audioWorkletNode = new AudioWorkletNode(offlineAudioContext, 'inspector-processor');
+                    const constantSourceNode = new ConstantSourceNode(offlineAudioContext);
                     const listener = spy();
 
                     audioWorkletNode.port.onmessage = listener;
@@ -102,6 +96,8 @@ describe('AudioWorklet', () => {
 
                         done();
                     }, 500);
+
+                    offlineAudioContext.startRendering();
                 });
         });
 
@@ -112,14 +108,14 @@ describe('AudioWorklet', () => {
         beforeEach(async function () {
             this.timeout(10000);
 
-            await audioContext.audioWorklet.addModule('base/test/fixtures/library.js');
-            await audioContext.audioWorklet.addModule('base/test/fixtures/dependent-processor.js');
+            await offlineAudioContext.audioWorklet.addModule('base/test/fixtures/library.js');
+            await offlineAudioContext.audioWorklet.addModule('base/test/fixtures/dependent-processor.js');
         });
 
         // bug #91
 
         it('should not persist the scope across calls to addModule()', (done) => {
-            const audioWorkletNode = new AudioWorkletNode(audioContext, 'dependent-processor');
+            const audioWorkletNode = new AudioWorkletNode(offlineAudioContext, 'dependent-processor');
 
             audioWorkletNode.port.onmessage = ({ data }) => {
                 audioWorkletNode.port.onmessage = null;
@@ -134,35 +130,32 @@ describe('AudioWorklet', () => {
 
     });
 
-    // @todo For some reason this test does currently not pass when running on BrowserStack.
-    if (!process.env.TRAVIS) { // eslint-disable-line no-undef
+    describe('with a failing processor', () => {
 
-        describe('with a failing processor', () => {
+        beforeEach(async function () {
+            this.timeout(10000);
 
-            beforeEach(async function () {
-                this.timeout(10000);
-
-                await audioContext.audioWorklet.addModule('base/test/fixtures/failing-processor.js');
-            });
-
-            // bug #156
-
-            it('should fire a regular event', function (done) {
-                this.timeout(10000);
-
-                const audioWorkletNode = new AudioWorkletNode(audioContext, 'failing-processor');
-
-                audioWorkletNode.onprocessorerror = function (event) {
-                    expect(event).to.be.not.an.instanceOf(ErrorEvent);
-
-                    done();
-                };
-
-                audioWorkletNode.connect(audioContext.destination);
-            });
-
+            await offlineAudioContext.audioWorklet.addModule('base/test/fixtures/failing-processor.js');
         });
 
-    }
+        // bug #156
+
+        it('should fire a regular event', function (done) {
+            this.timeout(10000);
+
+            const audioWorkletNode = new AudioWorkletNode(offlineAudioContext, 'failing-processor');
+
+            audioWorkletNode.onprocessorerror = function (event) {
+                expect(event).to.be.not.an.instanceOf(ErrorEvent);
+
+                done();
+            };
+
+            audioWorkletNode.connect(offlineAudioContext.destination);
+
+            offlineAudioContext.startRendering();
+        });
+
+    });
 
 });
