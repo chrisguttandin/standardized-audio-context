ed0f810fe98515bb8bb2e13af772d05eaa3de12c
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-08-22 13:23:01 +0200

    fix: use the AudioWorkletNodeOptions provided by TypeScript

--- src/types/native-audio-worklet-node-options.ts
+++ src/types/native-audio-worklet-node-options.ts
@@ -1,4 +1,1 @@
-import { IAudioWorkletNodeOptions } from '../interfaces';
-
-// @todo Since there are no native types yet, both types are the same.
-export type TNativeAudioWorkletNodeOptions = IAudioWorkletNodeOptions;
+export type TNativeAudioWorkletNodeOptions = AudioWorkletNodeOptions;
