602d777fb8cbfdc0661aa7b2924458037f009b7a
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-09-25 16:19:05 +0200

    test: update expectation tests for Firefox Developer v82

--- test/expectation/firefox/legacy/audio-context-constructor.js
+++ test/expectation/firefox/legacy/audio-context-constructor.js
@@ -38,6 +38,22 @@ describe('audioContextConstructor', () => {
                 }
             });
         });
+
+        // bug #157
+
+        describe('copyFromChannel()/copyToChannel()', () => {
+            let audioBuffer;
+
+            beforeEach(() => {
+                audioBuffer = audioContext.createBuffer(2, 100, 44100);
+            });
+
+            it('should not allow to copy values with a bufferOffset greater than the length of the AudioBuffer', () => {
+                const source = new Float32Array(10);
+
+                expect(() => audioBuffer.copyToChannel(source, 0, 101)).to.throw(Error);
+            });
+        });
     });
 
     describe('createBufferSource()', () => {
