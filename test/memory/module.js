cba74f152474dd199443b5f4ed8f42914fc492b5
Christoph Guttandin <chrisguttandin@media-codings.com>
2020-11-04 22:24:42 +0100

    test: handle error parameter

--- test/memory/module.js
+++ test/memory/module.js
@@ -20,7 +20,9 @@ const compileBundle = () => {
 
         compiler.outputFileSystem = memoryFileSystem;
         compiler.run((err, stats) => {
-            if (stats.hasErrors() || stats.hasWarnings()) {
+            if (err !== null) {
+                reject(err);
+            } else if (stats.hasErrors() || stats.hasWarnings()) {
                 reject(new Error(stats.toString({ errorDetails: true, warnings: true })));
             } else {
                 resolve(memoryFileSystem.readFileSync('/bundle.js', 'utf-8')); // eslint-disable-line node/no-sync
