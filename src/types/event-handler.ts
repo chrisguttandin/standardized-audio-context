3fc75375887516f101b71f2550d83f0d3be168e3
Christoph Guttandin <chrisguttandin@media-codings.com>
2021-05-27 14:13:10 +0200

    fix: apply ThisType on a temporary object

--- src/types/event-handler.ts
+++ src/types/event-handler.ts
@@ -1,1 +1,1 @@
-export type TEventHandler<T, U extends Event = Event> = ThisType<T> & ((event: U) => void);
+export type TEventHandler<T, U extends Event = Event> = (ThisType<T> & { handler(event: U): void })['handler'];
