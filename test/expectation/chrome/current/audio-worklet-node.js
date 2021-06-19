f6b0f7550975f86c8b03cc7b6f3478bb3a46a3fa
Christoph Guttandin <chrisguttandin@media-codings.com>
2021-06-19 17:55:27 +0200

    test: update expectation tests for Chrome Canary v93

--- test/expectation/chrome/current/audio-worklet-node.js
+++ test/expectation/chrome/current/audio-worklet-node.js
@@ -1,4 +1,27 @@
 describe('AudioWorklet', () => {
+    describe('with a processor with parameters', () => {
+        let offlineAudioContext;
+
+        beforeEach(async function () {
+            this.timeout(10000);
+
+            offlineAudioContext = new OfflineAudioContext(1, 256000, 44100);
+
+            await offlineAudioContext.audioWorklet.addModule('base/test/fixtures/gain-processor.js');
+        });
+
+        describe('without specified maxValue and minValue values', () => {
+            // bug #82
+
+            it('should be 3.402820018375656e+38 and -3.402820018375656e+38', () => {
+                const audioWorkletNode = new AudioWorkletNode(offlineAudioContext, 'gain-processor');
+
+                expect(audioWorkletNode.parameters.get('gain').maxValue).to.equal(3.402820018375656e38);
+                expect(audioWorkletNode.parameters.get('gain').minValue).to.equal(-3.402820018375656e38);
+            });
+        });
+    });
+
     describe('with a failing processor', () => {
         let offlineAudioContext;
 
