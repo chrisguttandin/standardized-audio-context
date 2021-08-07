e5ce31d95d48ad0a2368b146f4b9100f8e698484
Christoph Guttandin <chrisguttandin@media-codings.com>
2021-08-08 00:12:47 +0200

    fix: catch errors thrown when detaching buffers

--- src/helpers/detach-array-buffer.ts
+++ src/helpers/detach-array-buffer.ts
@@ -2,13 +2,21 @@ export const detachArrayBuffer = (arrayBuffer: ArrayBuffer): Promise<void> => {
     const { port1, port2 } = new MessageChannel();
 
     return new Promise((resolve) => {
-        port2.onmessage = () => {
+        const closeAndResolve = () => {
+            port2.onmessage = null;
+
             port1.close();
             port2.close();
 
             resolve();
         };
 
-        port1.postMessage(arrayBuffer, [arrayBuffer]);
+        port2.onmessage = () => closeAndResolve();
+
+        try {
+            port1.postMessage(arrayBuffer, [arrayBuffer]);
+        } finally {
+            closeAndResolve();
+        }
     });
 };
