d4751f4b4fdfbabb46a8f3f959cea0725fcbeca2
Christoph Guttandin <chrisguttandin@media-codings.com>
2021-08-30 17:08:09 +0200

    fix: redefine the native MediaStreamTrackAudioSourceNode

--- src/types/native-audio-context-constructor.ts
+++ src/types/native-audio-context-constructor.ts
@@ -1,1 +1,4 @@
-export type TNativeAudioContextConstructor = typeof AudioContext;
+import { TNativeAudioContext } from './native-audio-context';
+
+// @todo TypeScript v4.4 doesn't come with definitions for the MediaStreamTrackAudioSourceNode anymore.
+export type TNativeAudioContextConstructor = new (options?: AudioContextOptions) => TNativeAudioContext;
--- src/types/native-audio-context.ts
+++ src/types/native-audio-context.ts
@@ -1,1 +1,6 @@
-export type TNativeAudioContext = AudioContext;
+import { TNativeMediaStreamTrackAudioSourceNode } from './native-media-stream-track-audio-source-node';
+
+export type TNativeAudioContext = AudioContext & {
+    // @todo TypeScript v4.4 doesn't come with definitions for the MediaStreamTrackAudioSourceNode anymore.
+    createMediaStreamTrackSource(mediaStreamTrack: MediaStreamTrack): TNativeMediaStreamTrackAudioSourceNode;
+};
