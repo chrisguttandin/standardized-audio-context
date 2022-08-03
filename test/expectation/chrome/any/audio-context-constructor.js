105fa3a076f150b4ab31a69c295a7b26b4efb332
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-08-03 17:35:15 +0200

    test: update expectation tests for Chrome Canary v106

--- test/expectation/chrome/any/audio-context-constructor.js
+++ test/expectation/chrome/any/audio-context-constructor.js
@@ -93,14 +93,6 @@ describe('audioContextConstructor', () => {
         });
     });
 
-    describe('outputLatency', () => {
-        // bug #40
-
-        it('should not be implemented', () => {
-            expect(audioContext.outputLatency).to.be.undefined;
-        });
-    });
-
     describe('state', () => {
         // @todo For some reason this test does currently not pass when running on BrowserStack.
         // eslint-disable-next-line no-undef
@@ -153,26 +145,6 @@ describe('audioContextConstructor', () => {
 
                 done();
             }, 1000);
-        });
-    });
-
-    describe('resume()', () => {
-        afterEach(() => {
-            // Create a closeable AudioContext to align the behaviour with other tests.
-            audioContext = new AudioContext();
-        });
-
-        beforeEach(() => audioContext.close());
-
-        // bug #55
-
-        it('should throw an InvalidAccessError with a closed AudioContext', (done) => {
-            audioContext.resume().catch((err) => {
-                expect(err.code).to.equal(15);
-                expect(err.name).to.equal('InvalidAccessError');
-
-                done();
-            });
         });
     });
 });
--- test/expectation/chrome/legacy/audio-context-constructor.js
+++ test/expectation/chrome/legacy/audio-context-constructor.js
@@ -7,6 +7,14 @@ describe('audioContextConstructor', () => {
         audioContext = new AudioContext();
     });
 
+    describe('outputLatency', () => {
+        // bug #40
+
+        it('should not be implemented', () => {
+            expect(audioContext.outputLatency).to.be.undefined;
+        });
+    });
+
     describe('createAnalyser()', () => {
         // bug #58
 
@@ -38,6 +46,26 @@ describe('audioContextConstructor', () => {
                 expect(() => audioBufferSourceNode.stop(-1))
                     .to.throw(DOMException)
                     .with.property('name', 'InvalidStateError');
+            });
+        });
+    });
+
+    describe('resume()', () => {
+        afterEach(() => {
+            // Create a closeable AudioContext to align the behaviour with other tests.
+            audioContext = new AudioContext();
+        });
+
+        beforeEach(() => audioContext.close());
+
+        // bug #55
+
+        it('should throw an InvalidAccessError with a closed AudioContext', (done) => {
+            audioContext.resume().catch((err) => {
+                expect(err.code).to.equal(15);
+                expect(err.name).to.equal('InvalidAccessError');
+
+                done();
             });
         });
     });
