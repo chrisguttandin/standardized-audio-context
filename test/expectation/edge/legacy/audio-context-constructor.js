c8fd773839feb3992b1d278580c3b4a2b1e9df26
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-08-03 17:40:58 +0200

    test: update expectation tests for Edge v103

--- test/expectation/edge/legacy/audio-context-constructor.js
+++ test/expectation/edge/legacy/audio-context-constructor.js
@@ -7,6 +7,14 @@ describe('audioContextConstructor', () => {
         audioContext = new AudioContext();
     });
 
+    describe('outputLatency', () => {
+        // bug #40
+
+        it('should not be implemented', () => {
+            expect(audioContext.outputLatency).to.be.undefined;
+        });
+    });
+
     describe('createAnalyser()', () => {
         // bug #58
 
@@ -25,6 +33,26 @@ describe('audioContextConstructor', () => {
             } finally {
                 anotherAudioContext.close();
             }
+        });
+    });
+
+    describe('resume()', () => {
+        afterEach(() => {
+            // Create a closeable AudioContext to align the behaviour with other tests.
+            audioContext = new AudioContext();
+        });
+
+        beforeEach(() => audioContext.close());
+
+        // bug #55
+
+        it('should throw an InvalidAccessError with a closed AudioContext', (done) => {
+            audioContext.resume().catch((err) => {
+                expect(err.code).to.equal(15);
+                expect(err.name).to.equal('InvalidAccessError');
+
+                done();
+            });
         });
     });
 });
