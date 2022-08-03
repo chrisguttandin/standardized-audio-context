78a92331e403b14f2604c6f29204f45cb619d34c
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-08-03 17:37:39 +0200

    test: update expectation tests for Chrome v103

--- test/expectation/chrome/current/audio-context-constructor.js
+++ test/expectation/chrome/current/audio-context-constructor.js
@@ -7,14 +7,6 @@ describe('audioContextConstructor', () => {
         audioContext = new AudioContext();
     });
 
-    describe('outputLatency', () => {
-        // bug #40
-
-        it('should not be implemented', () => {
-            expect(audioContext.outputLatency).to.be.undefined;
-        });
-    });
-
     describe('createBufferSource()', () => {
         describe('stop()', () => {
             // bug #44
@@ -25,26 +17,6 @@ describe('audioContextConstructor', () => {
                 expect(() => audioBufferSourceNode.stop(-1))
                     .to.throw(DOMException)
                     .with.property('name', 'InvalidStateError');
-            });
-        });
-    });
-
-    describe('resume()', () => {
-        afterEach(() => {
-            // Create a closeable AudioContext to align the behaviour with other tests.
-            audioContext = new AudioContext();
-        });
-
-        beforeEach(() => audioContext.close());
-
-        // bug #55
-
-        it('should throw an InvalidAccessError with a closed AudioContext', (done) => {
-            audioContext.resume().catch((err) => {
-                expect(err.code).to.equal(15);
-                expect(err.name).to.equal('InvalidAccessError');
-
-                done();
             });
         });
     });
