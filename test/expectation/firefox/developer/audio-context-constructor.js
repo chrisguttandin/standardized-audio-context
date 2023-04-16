373ebb7918eadde76b250094663529cab08552dd
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-04-16 16:35:59 +0200

    test: update expectation tests for Firefox Developer v113

--- test/expectation/firefox/developer/audio-context-constructor.js
+++ test/expectation/firefox/developer/audio-context-constructor.js
@@ -9,30 +9,14 @@ describe('audioContextConstructor', () => {
 
     describe('audioWorklet', () => {
         describe('addModule()', () => {
-            describe('with a module which contains an import statement', () => {
-                // bug #176
-
-                it('should throw an error', function (done) {
-                    this.timeout(10000);
-
-                    audioContext.audioWorklet.addModule('base/test/fixtures/gibberish-processor.js').catch((err) => {
-                        expect(err.code).to.equal(20);
-                        expect(err.name).to.equal('AbortError');
-
-                        done();
-                    });
-                });
-            });
-
             describe('with an unparsable module', () => {
-                // bug #177
+                // bug #182
 
-                it('should return a promise which rejects an AbortError', function (done) {
+                it('should return a promise which rejects a SyntaxError', function (done) {
                     this.timeout(10000);
 
                     audioContext.audioWorklet.addModule('base/test/fixtures/unparsable-processor.xs').catch((err) => {
-                        expect(err.code).to.equal(20);
-                        expect(err.name).to.equal('AbortError');
+                        expect(err).to.be.an.instanceOf(SyntaxError);
 
                         done();
                     });
