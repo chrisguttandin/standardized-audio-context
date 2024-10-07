af7635e13d3e53f993a63813ef395301761e9d15
Christoph Guttandin <chrisguttandin@media-codings.com>
2024-10-07 21:10:35 +0200

    test: remove obsolete grouping

--- test/expectation/safari/legacy/audio-context-constructor.js
+++ test/expectation/safari/legacy/audio-context-constructor.js
@@ -3,222 +3,220 @@ describe('audioContextConstructor', () => {
 
     afterEach(() => audioContext.close());
 
-    describe('with a constructed AudioContext', () => {
+    beforeEach(() => {
+        audioContext = new AudioContext();
+    });
+
+    describe('createBiquadFilter()', () => {
+        let biquadFilterNode;
+
         beforeEach(() => {
-            audioContext = new AudioContext();
+            biquadFilterNode = audioContext.createBiquadFilter();
         });
 
-        describe('createBiquadFilter()', () => {
-            let biquadFilterNode;
+        describe('getFrequencyResponse()', () => {
+            // bug #189
+            it('should throw an InvalidStateError', (done) => {
+                try {
+                    biquadFilterNode.getFrequencyResponse(new Float32Array(), new Float32Array(1), new Float32Array(1));
+                } catch (err) {
+                    expect(err.code).to.equal(11);
+                    expect(err.name).to.equal('InvalidStateError');
 
-            beforeEach(() => {
-                biquadFilterNode = audioContext.createBiquadFilter();
+                    done();
+                }
             });
+        });
+    });
 
-            describe('getFrequencyResponse()', () => {
-                // bug #189
-                it('should throw an InvalidStateError', (done) => {
-                    try {
-                        biquadFilterNode.getFrequencyResponse(new Float32Array(), new Float32Array(1), new Float32Array(1));
-                    } catch (err) {
-                        expect(err.code).to.equal(11);
-                        expect(err.name).to.equal('InvalidStateError');
+    describe('createMediaStreamSource()', () => {
+        // @todo There is currently no way to disable the autoplay policy on BrowserStack or Sauce Labs.
+        // eslint-disable-next-line no-undef
+        if (!process.env.CI) {
+            describe('with a mediaStream with two audio tracks', () => {
+                let audioBufferSourceNode;
+                let gainNodes;
+                let mediaStream;
+                let mediaStreamAudioDestinationNodes;
 
-                        done();
+                afterEach(() => {
+                    audioBufferSourceNode.stop();
+
+                    audioBufferSourceNode.disconnect(gainNodes[0]);
+                    audioBufferSourceNode.disconnect(gainNodes[1]);
+                    gainNodes[0].disconnect(mediaStreamAudioDestinationNodes[0]);
+                    gainNodes[1].disconnect(mediaStreamAudioDestinationNodes[1]);
+                });
+
+                beforeEach(() => {
+                    audioBufferSourceNode = audioContext.createBufferSource();
+                    gainNodes = [audioContext.createGain(), audioContext.createGain()];
+                    mediaStreamAudioDestinationNodes = [
+                        audioContext.createMediaStreamDestination(),
+                        audioContext.createMediaStreamDestination()
+                    ];
+
+                    const audioBuffer = audioContext.createBuffer(1, 2, audioContext.sampleRate);
+
+                    audioBuffer.getChannelData(0)[0] = 1;
+                    audioBuffer.getChannelData(0)[1] = 1;
+
+                    audioBufferSourceNode.buffer = audioBuffer;
+                    audioBufferSourceNode.loop = true;
+
+                    audioBufferSourceNode.connect(gainNodes[0]).connect(mediaStreamAudioDestinationNodes[0]);
+                    audioBufferSourceNode.connect(gainNodes[1]).connect(mediaStreamAudioDestinationNodes[1]);
+
+                    audioBufferSourceNode.start();
+
+                    const audioStreamTracks = mediaStreamAudioDestinationNodes.map(({ stream }) => stream.getAudioTracks()[0]);
+
+                    if (audioStreamTracks[0].id > audioStreamTracks[1].id) {
+                        mediaStream = mediaStreamAudioDestinationNodes[0].stream;
+
+                        gainNodes[0].gain.value = 0;
+                        mediaStream.addTrack(audioStreamTracks[1]);
+                    } else {
+                        mediaStream = mediaStreamAudioDestinationNodes[1].stream;
+
+                        gainNodes[1].gain.value = 0;
+                        mediaStream.addTrack(audioStreamTracks[0]);
                     }
                 });
-            });
-        });
 
-        describe('createMediaStreamSource()', () => {
-            // @todo There is currently no way to disable the autoplay policy on BrowserStack or Sauce Labs.
-            // eslint-disable-next-line no-undef
-            if (!process.env.CI) {
-                describe('with a mediaStream with two audio tracks', () => {
-                    let audioBufferSourceNode;
-                    let gainNodes;
-                    let mediaStream;
-                    let mediaStreamAudioDestinationNodes;
+                // bug #159
 
-                    afterEach(() => {
-                        audioBufferSourceNode.stop();
+                it('should pick the first track', (done) => {
+                    const mediaStreamAudioSourceNode = audioContext.createMediaStreamSource(mediaStream);
+                    const scriptProcessorNode = audioContext.createScriptProcessor(512);
 
-                        audioBufferSourceNode.disconnect(gainNodes[0]);
-                        audioBufferSourceNode.disconnect(gainNodes[1]);
-                        gainNodes[0].disconnect(mediaStreamAudioDestinationNodes[0]);
-                        gainNodes[1].disconnect(mediaStreamAudioDestinationNodes[1]);
-                    });
+                    let numberOfInvocations = 0;
 
-                    beforeEach(() => {
-                        audioBufferSourceNode = audioContext.createBufferSource();
-                        gainNodes = [audioContext.createGain(), audioContext.createGain()];
-                        mediaStreamAudioDestinationNodes = [
-                            audioContext.createMediaStreamDestination(),
-                            audioContext.createMediaStreamDestination()
-                        ];
+                    scriptProcessorNode.onaudioprocess = ({ inputBuffer }) => {
+                        numberOfInvocations += 1;
 
-                        const audioBuffer = audioContext.createBuffer(1, 2, audioContext.sampleRate);
+                        if (numberOfInvocations > 10) {
+                            const channelData = inputBuffer.getChannelData(0);
 
-                        audioBuffer.getChannelData(0)[0] = 1;
-                        audioBuffer.getChannelData(0)[1] = 1;
+                            for (let i = 0; i < 512; i += 1) {
+                                if (channelData[i] !== 0) {
+                                    mediaStreamAudioSourceNode.disconnect(scriptProcessorNode);
+                                    scriptProcessorNode.disconnect(audioContext.destination);
 
-                        audioBufferSourceNode.buffer = audioBuffer;
-                        audioBufferSourceNode.loop = true;
+                                    done(new Error('The signal is expected to be zero at all time.'));
 
-                        audioBufferSourceNode.connect(gainNodes[0]).connect(mediaStreamAudioDestinationNodes[0]);
-                        audioBufferSourceNode.connect(gainNodes[1]).connect(mediaStreamAudioDestinationNodes[1]);
-
-                        audioBufferSourceNode.start();
-
-                        const audioStreamTracks = mediaStreamAudioDestinationNodes.map(({ stream }) => stream.getAudioTracks()[0]);
-
-                        if (audioStreamTracks[0].id > audioStreamTracks[1].id) {
-                            mediaStream = mediaStreamAudioDestinationNodes[0].stream;
-
-                            gainNodes[0].gain.value = 0;
-                            mediaStream.addTrack(audioStreamTracks[1]);
-                        } else {
-                            mediaStream = mediaStreamAudioDestinationNodes[1].stream;
-
-                            gainNodes[1].gain.value = 0;
-                            mediaStream.addTrack(audioStreamTracks[0]);
+                                    break;
+                                }
+                            }
                         }
-                    });
+                    };
 
-                    // bug #159
+                    mediaStreamAudioSourceNode.connect(scriptProcessorNode).connect(audioContext.destination);
 
-                    it('should pick the first track', (done) => {
-                        const mediaStreamAudioSourceNode = audioContext.createMediaStreamSource(mediaStream);
-                        const scriptProcessorNode = audioContext.createScriptProcessor(512);
+                    setTimeout(() => {
+                        mediaStreamAudioSourceNode.disconnect(scriptProcessorNode);
+                        scriptProcessorNode.disconnect(audioContext.destination);
 
-                        let numberOfInvocations = 0;
+                        expect(numberOfInvocations).to.be.above(10);
 
-                        scriptProcessorNode.onaudioprocess = ({ inputBuffer }) => {
-                            numberOfInvocations += 1;
+                        done();
+                    }, 1000);
+                });
+            });
 
-                            if (numberOfInvocations > 10) {
-                                const channelData = inputBuffer.getChannelData(0);
+            describe('with two instances using the same mediaStream', () => {
+                let audioBufferSourceNode;
+                let mediaStream;
+                let mediaStreamAudioDestinationNode;
 
-                                for (let i = 0; i < 512; i += 1) {
-                                    if (channelData[i] !== 0) {
-                                        mediaStreamAudioSourceNode.disconnect(scriptProcessorNode);
-                                        scriptProcessorNode.disconnect(audioContext.destination);
+                afterEach(() => {
+                    audioBufferSourceNode.stop();
 
-                                        done(new Error('The signal is expected to be zero at all time.'));
-
-                                        break;
-                                    }
-                                }
-                            }
-                        };
-
-                        mediaStreamAudioSourceNode.connect(scriptProcessorNode).connect(audioContext.destination);
-
-                        setTimeout(() => {
-                            mediaStreamAudioSourceNode.disconnect(scriptProcessorNode);
-                            scriptProcessorNode.disconnect(audioContext.destination);
-
-                            expect(numberOfInvocations).to.be.above(10);
-
-                            done();
-                        }, 1000);
-                    });
+                    audioBufferSourceNode.disconnect(mediaStreamAudioDestinationNode);
                 });
 
-                describe('with two instances using the same mediaStream', () => {
-                    let audioBufferSourceNode;
-                    let mediaStream;
-                    let mediaStreamAudioDestinationNode;
+                beforeEach(() => {
+                    audioBufferSourceNode = audioContext.createBufferSource();
+                    mediaStreamAudioDestinationNode = audioContext.createMediaStreamDestination();
 
-                    afterEach(() => {
-                        audioBufferSourceNode.stop();
+                    const audioBuffer = audioContext.createBuffer(1, 2, audioContext.sampleRate);
 
-                        audioBufferSourceNode.disconnect(mediaStreamAudioDestinationNode);
-                    });
+                    audioBuffer.getChannelData(0)[0] = 1;
+                    audioBuffer.getChannelData(0)[1] = 1;
 
-                    beforeEach(() => {
-                        audioBufferSourceNode = audioContext.createBufferSource();
-                        mediaStreamAudioDestinationNode = audioContext.createMediaStreamDestination();
+                    audioBufferSourceNode.buffer = audioBuffer;
+                    audioBufferSourceNode.loop = true;
 
-                        const audioBuffer = audioContext.createBuffer(1, 2, audioContext.sampleRate);
+                    audioBufferSourceNode.connect(mediaStreamAudioDestinationNode);
 
-                        audioBuffer.getChannelData(0)[0] = 1;
-                        audioBuffer.getChannelData(0)[1] = 1;
+                    audioBufferSourceNode.start();
 
-                        audioBufferSourceNode.buffer = audioBuffer;
-                        audioBufferSourceNode.loop = true;
+                    mediaStream = mediaStreamAudioDestinationNode.stream;
+                });
 
-                        audioBufferSourceNode.connect(mediaStreamAudioDestinationNode);
+                // bug #201
 
-                        audioBufferSourceNode.start();
+                it('should only output sound for the first node', (done) => {
+                    const channelData = new Float32Array(512);
+                    const channelMergerNode = audioContext.createChannelMerger(2);
+                    const mediaStreamAudioSourceNodeA = audioContext.createMediaStreamSource(mediaStream);
+                    const mediaStreamAudioSourceNodeB = audioContext.createMediaStreamSource(mediaStream);
+                    const scriptProcessorNode = audioContext.createScriptProcessor(512);
 
-                        mediaStream = mediaStreamAudioDestinationNode.stream;
-                    });
+                    let numberOfInvocations = 0;
 
-                    // bug #201
+                    scriptProcessorNode.onaudioprocess = ({ inputBuffer }) => {
+                        numberOfInvocations += 1;
 
-                    it('should only output sound for the first node', (done) => {
-                        const channelData = new Float32Array(512);
-                        const channelMergerNode = audioContext.createChannelMerger(2);
-                        const mediaStreamAudioSourceNodeA = audioContext.createMediaStreamSource(mediaStream);
-                        const mediaStreamAudioSourceNodeB = audioContext.createMediaStreamSource(mediaStream);
-                        const scriptProcessorNode = audioContext.createScriptProcessor(512);
+                        if (numberOfInvocations > 1) {
+                            inputBuffer.copyFromChannel(channelData, 0);
 
-                        let numberOfInvocations = 0;
+                            for (let i = 0; i < 512; i += 1) {
+                                if (channelData[i] !== 1) {
+                                    channelMergerNode.disconnect(scriptProcessorNode);
+                                    mediaStreamAudioSourceNodeA.disconnect(channelMergerNode);
+                                    mediaStreamAudioSourceNodeB.disconnect(channelMergerNode);
+                                    scriptProcessorNode.disconnect(audioContext.destination);
 
-                        scriptProcessorNode.onaudioprocess = ({ inputBuffer }) => {
-                            numberOfInvocations += 1;
+                                    done(new Error('The signal is expected to be one at all time.'));
 
-                            if (numberOfInvocations > 1) {
-                                inputBuffer.copyFromChannel(channelData, 0);
-
-                                for (let i = 0; i < 512; i += 1) {
-                                    if (channelData[i] !== 1) {
-                                        channelMergerNode.disconnect(scriptProcessorNode);
-                                        mediaStreamAudioSourceNodeA.disconnect(channelMergerNode);
-                                        mediaStreamAudioSourceNodeB.disconnect(channelMergerNode);
-                                        scriptProcessorNode.disconnect(audioContext.destination);
-
-                                        done(new Error('The signal is expected to be one at all time.'));
-
-                                        break;
-                                    }
-                                }
-
-                                inputBuffer.copyFromChannel(channelData, 1);
-
-                                for (let i = 0; i < 512; i += 1) {
-                                    if (channelData[i] !== 0) {
-                                        channelMergerNode.disconnect(scriptProcessorNode);
-                                        mediaStreamAudioSourceNodeA.disconnect(channelMergerNode);
-                                        mediaStreamAudioSourceNodeB.disconnect(channelMergerNode);
-                                        scriptProcessorNode.disconnect(audioContext.destination);
-
-                                        done(new Error('The signal is expected to be zero at all time.'));
-
-                                        break;
-                                    }
+                                    break;
                                 }
                             }
-                        };
 
-                        mediaStreamAudioSourceNodeA.connect(channelMergerNode, 0, 0);
-                        mediaStreamAudioSourceNodeB.connect(channelMergerNode, 0, 1);
-                        channelMergerNode.connect(scriptProcessorNode).connect(audioContext.destination);
+                            inputBuffer.copyFromChannel(channelData, 1);
 
-                        setTimeout(() => {
-                            channelMergerNode.disconnect(scriptProcessorNode);
-                            mediaStreamAudioSourceNodeA.disconnect(channelMergerNode);
-                            mediaStreamAudioSourceNodeB.disconnect(channelMergerNode);
-                            scriptProcessorNode.disconnect(audioContext.destination);
+                            for (let i = 0; i < 512; i += 1) {
+                                if (channelData[i] !== 0) {
+                                    channelMergerNode.disconnect(scriptProcessorNode);
+                                    mediaStreamAudioSourceNodeA.disconnect(channelMergerNode);
+                                    mediaStreamAudioSourceNodeB.disconnect(channelMergerNode);
+                                    scriptProcessorNode.disconnect(audioContext.destination);
 
-                            expect(numberOfInvocations).to.be.above(1);
+                                    done(new Error('The signal is expected to be zero at all time.'));
 
-                            done();
-                        }, 1000);
-                    });
+                                    break;
+                                }
+                            }
+                        }
+                    };
+
+                    mediaStreamAudioSourceNodeA.connect(channelMergerNode, 0, 0);
+                    mediaStreamAudioSourceNodeB.connect(channelMergerNode, 0, 1);
+                    channelMergerNode.connect(scriptProcessorNode).connect(audioContext.destination);
+
+                    setTimeout(() => {
+                        channelMergerNode.disconnect(scriptProcessorNode);
+                        mediaStreamAudioSourceNodeA.disconnect(channelMergerNode);
+                        mediaStreamAudioSourceNodeB.disconnect(channelMergerNode);
+                        scriptProcessorNode.disconnect(audioContext.destination);
+
+                        expect(numberOfInvocations).to.be.above(1);
+
+                        done();
+                    }, 1000);
                 });
-            }
-        });
+            });
+        }
     });
 });
