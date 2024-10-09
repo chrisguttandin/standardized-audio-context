47e7f28400fc5833df8dff22ef5aff3f3aa5339c
Christoph Guttandin <chrisguttandin@media-codings.com>
2024-10-10 00:10:02 +0200

    test: remove check for incorrect maxValue and minValue values

--- src/factories/test-audio-node-connect-method-verification-support.ts
+++ src/factories/test-audio-node-connect-method-verification-support.ts
@@ -2,7 +2,9 @@ import { TNativeOfflineAudioContextConstructor } from '../types';
 
 /*
  * Bug #58: Chrome up to version 92 threw no InvalidAccessError when connecting an AudioParam to an AudioNode belonging to a different
- * AudioContext.
+ * AudioContext. It also had one more bug but since this is easy to test it's used here as a placeholder.
+ *
+ * Bug #82: Chrome up to version 92 exposed slightly different maxValue and minValue values of an AudioParam of an AudioWorkletNode.
  */
 export const createTestAudioNodeConnectMethodVerificationSupport =
     (nativeOfflineAudioContextConstructor: null | TNativeOfflineAudioContextConstructor) => () => {
