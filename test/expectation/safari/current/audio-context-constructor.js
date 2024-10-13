4b828662875b5ee1b80408685c16b75374a896b4
Christoph Guttandin <chrisguttandin@media-codings.com>
2024-10-14 00:46:54 +0200

    test: remove obsolete expectation test

--- test/expectation/safari/current/audio-context-constructor.js
+++ test/expectation/safari/current/audio-context-constructor.js
@@ -10,27 +10,6 @@ describe('audioContextConstructor', () => {
             audioContext = new AudioContext();
         });
 
-        describe('state', () => {
-            // bug #188
-
-            // This test will only work when changing the browser settings to allow popups.
-            it("should set the state of the context to 'interrupted'", (done) => {
-                const oscillator = audioContext.createOscillator();
-                const currentTime = audioContext.currentTime;
-
-                oscillator.start(currentTime);
-                oscillator.stop(currentTime + 0.1);
-
-                window.open('about:blank', '_blank');
-
-                setTimeout(() => {
-                    expect(audioContext.state).to.equal('interrupted');
-
-                    done();
-                }, 500);
-            });
-        });
-
         // Bug #198
 
         describe('getOutputTimestamp()', () => {
