bac3305635f04863b6bd12993e8ac12701eb8a8a
Christoph Guttandin <chrisguttandin@media-codings.com>
2024-10-30 00:06:35 +0100

    test: update expectation tests for Firefox v132

--- config/karma/config-expectation-firefox-penultimate.js
+++ config/karma/config-expectation-firefox-penultimate.js
@@ -23,7 +23,7 @@ module.exports = (config) => {
         customLaunchers: {
             penultimateFirefoxHeadless: {
                 base: 'FirefoxHeadless',
-                command: 'firefox-v129/firefox/Firefox.app/Contents/MacOS/firefox'
+                command: 'firefox-v130/firefox/Firefox.app/Contents/MacOS/firefox'
             }
         },
 
