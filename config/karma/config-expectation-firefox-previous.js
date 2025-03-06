993bdd4581364dfe4ab1c4e177e3708e99c36b27
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-03-06 20:37:50 +0100

    test: update expectation tests for Firefox v136

--- config/karma/config-expectation-firefox-previous.js
+++ config/karma/config-expectation-firefox-previous.js
@@ -23,7 +23,7 @@ module.exports = (config) => {
         customLaunchers: {
             PreviousFirefoxHeadless: {
                 base: 'FirefoxHeadless',
-                command: 'firefox-v134/firefox/Firefox.app/Contents/MacOS/firefox'
+                command: 'firefox-v135/firefox/Firefox.app/Contents/MacOS/firefox'
             }
         },
 
