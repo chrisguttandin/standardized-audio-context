9ad92dade517c4b685acc4a5a2d381b1d037b62c
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-03-20 22:12:04 +0100

    build(config): add test scripts

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -1,56 +1,52 @@
-module.exports = (grunt) => {
-    const continuous = grunt.option('continuous') === true;
-
+module.exports = () => {
     return {
         'build': {
             cmd: 'npm run build'
         },
         'test-expectation-chrome': {
-            cmd: `karma start config/karma/config-expectation-chrome.js ${continuous ? '--concurrency Infinity' : '--single-run'}`
+            cmd: 'npm run test:expectation-chrome'
         },
         'test-expectation-chrome-canary': {
-            cmd: `karma start config/karma/config-expectation-chrome-canary.js ${continuous ? '--concurrency Infinity' : '--single-run'}`
+            cmd: 'npm run test:expectation-chrome-canary'
         },
         'test-expectation-chrome-penultimate': {
-            cmd: `karma start config/karma/config-expectation-chrome-penultimate.js ${continuous ? '--concurrency Infinity' : '--single-run'}`
+            cmd: 'npm run test:expectation-chrome-penultimate'
         },
         'test-expectation-chrome-previous': {
-            cmd: `karma start config/karma/config-expectation-chrome-previous.js ${continuous ? '--concurrency Infinity' : '--single-run'}`
+            cmd: 'npm run test:expectation-chrome-previous'
         },
         'test-expectation-firefox': {
-            cmd: `karma start config/karma/config-expectation-firefox.js ${continuous ? '--concurrency Infinity' : '--single-run'}`
+            cmd: 'npm run test:expectation-firefox'
         },
         'test-expectation-firefox-developer': {
-            cmd: `karma start config/karma/config-expectation-firefox-developer.js ${
-                continuous ? '--concurrency Infinity' : '--single-run'
-            }`
+            cmd: 'npm run test:expectation-firefox-developer'
         },
         'test-expectation-firefox-penultimate': {
-            cmd: `karma start config/karma/config-expectation-firefox-penultimate.js ${continuous ? '--concurrency Infinity' : '--single-run'}`
+            cmd: 'npm run test:expectation-firefox-penultimate'
         },
         'test-expectation-firefox-previous': {
-            cmd: `karma start config/karma/config-expectation-firefox-previous.js ${continuous ? '--concurrency Infinity' : '--single-run'}`
+            cmd: 'npm run test:expectation-firefox-previous'
         },
         'test-expectation-safari': {
-            cmd: `karma start config/karma/config-expectation-safari.js ${continuous ? '--concurrency Infinity' : '--single-run'}`
+            cmd: 'npm run test:expectation-safari'
         },
         'test-expectation-safari-penultimate': {
-            cmd: `karma start config/karma/config-expectation-safari-penultimate.js ${continuous ? '--concurrency Infinity' : '--single-run'}`
+            cmd: 'npm run test:expectation-safari-penultimate'
         },
         'test-expectation-safari-previous': {
-            cmd: `karma start config/karma/config-expectation-safari-previous.js ${continuous ? '--concurrency Infinity' : '--single-run'}`
+            cmd: 'npm run test:expectation-safari-previous'
         },
         'test-integration-browser': {
-            cmd: `karma start config/karma/config-integration.js ${continuous ? '--concurrency Infinity' : '--single-run'}`
+            cmd: 'npm run test:integration-browser'
         },
         'test-integration-node': {
-            cmd: 'mocha --bail --parallel --recursive --require config/mocha/config-integration.js test/integration'
+            cmd: 'npm run test:integration-node'
         },
         'test-memory': {
-            cmd: 'mocha --bail --parallel --recursive --require config/mocha/config-memory.js test/memory'
+            cmd: 'npm run test:memory'
         },
         'test-unit': {
-            cmd: `karma start config/karma/config-unit.js ${continuous ? '--concurrency Infinity' : '--single-run'}`
+            cmd: 'npm run test:unit'
         }
     };
 };
