90f3142d7086b4358abe481f99992c5fe1f3cf80
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-03-23 21:18:19 +0100

    build(config): use npm scripts to execute tests

--- gruntfile.js
+++
@@ -1,5 +0,0 @@
-const loadGruntConfig = require('load-grunt-config');
-const { join } = require('path');
-const { cwd } = require('process');
-
-module.exports = (grunt) => loadGruntConfig(grunt, { configPath: join(cwd(), 'config/grunt') });
