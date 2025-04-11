74616e20438d029011671264c781b2aec34f66af
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-04-11 02:05:00 +0200

    test: update expectation tests for Chrome v135

--- config/karma/config-expectation-chrome-penultimate.js
+++ config/karma/config-expectation-chrome-penultimate.js
@@ -85,5 +85,5 @@ module.exports = (config) => {
         }
     });
 
-    env.CHROME_BIN = 'chromium-v132/chrome-mac/Chromium.app/Contents/MacOS/Chromium';
+    env.CHROME_BIN = 'chromium-v133/chrome-mac/Chromium.app/Contents/MacOS/Chromium';
 };
