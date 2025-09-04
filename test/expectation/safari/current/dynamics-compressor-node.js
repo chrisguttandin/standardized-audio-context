3370309af13e51bcd15e171db5069112ed163993
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-09-04 15:53:18 +0200

    test: add expectation test for DynamicsCompressorNode processing

---
+++ test/expectation/safari/current/dynamics-compressor-node.js
@@ -0,0 +1,25 @@
+describe('DynamicsCompressorNode', () => {
+    // bug #205
+
+    it('should be actively processing all the time', async () => {
+        const offlineAudioContext = new OfflineAudioContext({ length: 48294, sampleRate: 48000 });
+        const buffer = new AudioBuffer({ length: 5, sampleRate: 48000 });
+        const channelData = new Float32Array(Array.from({ length: 5 }, () => 1));
+
+        buffer.copyToChannel(channelData, 0);
+
+        const audioBufferSourceNode = new AudioBufferSourceNode(offlineAudioContext, { buffer });
+        const dynamicsCompressorNode = new DynamicsCompressorNode(offlineAudioContext);
+
+        audioBufferSourceNode.connect(dynamicsCompressorNode).connect(offlineAudioContext.destination);
+        audioBufferSourceNode.start(1);
+
+        const renderedBuffer = await offlineAudioContext.startRendering();
+
+        renderedBuffer.copyFromChannel(channelData, 0, 48288);
+
+        expect(Array.from(channelData)).to.deep.equal([
+            0.9443932175636292, 0.9444449543952942, 0.9444966912269592, 0.9445484280586243, 0.9446001648902893
+        ]);
+    });
+});
