94f5439e99e05ebf506a6c60ab4f20ae38335a24
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-12-15 11:05:37 +0100

    test: update expectation tests for Chrome v143

--- config/karma/config-expectation-chrome-previous.js
+++ config/karma/config-expectation-chrome-previous.js
@@ -85,5 +85,5 @@ module.exports = (config) => {
         }
     });
 
-    env.CHROME_BIN = 'chromium-v141/chrome-mac/Chromium.app/Contents/MacOS/Chromium';
+    env.CHROME_BIN = 'chromium-v142/chrome-mac/Chromium.app/Contents/MacOS/Chromium';
 };
